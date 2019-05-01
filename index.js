3ec4e902d58c5ae739ea3927cebdc516aeacc3d7
Thysce <trensetim@gmail.com>
2019-05-01 12:28:20 +0200

    Initial Commit
    Uploaded first working Franz Integration for pushover

---
+++ index.js
@@ -0,0 +1,3 @@
+// copied from https://github.com/meetfranz/plugins/blob/master/docs/integration.md [1.mai.2019]
+// default integration (e.g messenger.com, ...)
+module.exports = Franz => Franz;
---
+++ webview.js
@@ -0,0 +1,1 @@
+// copied from https://github.com/meetfranz/plugins/blob/master/docs/integration.md [1.mai.2019]
